59a0beff2e6e72f157ac3e6256f0702c25fdea77
Petr Novotný <krkabol@gmail.com>
2024-09-30 15:52:39 +0200

    OAuth nearly wokrs

--- htdocs/src/Controller/HomeController.php
+++ htdocs/src/Controller/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         // the template path is the relative file path from `templates/`
@@ -17,6 +18,17 @@ class HomeController extends AbstractController
             // where the key is the variable name and the value is the variable value
             // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
 
+        ]);
+    }
+
+    #[Route('/api/test', name: 'api_test')]
+    public function apiTest(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        return $this->json([
+            'message' => 'You successfully authenticated!',
+            'email' => $user->getEmail(),
         ]);
     }
 }
