7a21409e1dd0a7675bee81d1548f1b13d2ccb854
Petr Novotný <krkabol@gmail.com>
2024-09-30 11:35:36 +0200

    basic templating

--- htdocs/src/Controller/HomeController.php
+++ htdocs/src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         // the template path is the relative file path from `templates/`
-        return $this->render('base.html.twig', [
+        return $this->render('front/home/default.html.twig', [
             // this array defines the variables passed to the template,
             // where the key is the variable name and the value is the variable value
             // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
