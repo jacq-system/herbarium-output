e58db645e39ad0dbb5ba63e05fc15ca20f532717
Petr Novotný <krkabol@gmail.com>
2024-09-30 11:27:58 +0200

    add Home

---
+++ htdocs/src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        // the template path is the relative file path from `templates/`
+        return $this->render('base.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+
+        ]);
+    }
+}
