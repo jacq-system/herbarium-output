c75cba07c6aa136616435f7d89417321584b92c8
Petr Novotný <krkabol@gmail.com>
2024-11-12 16:00:16 +0100

    route classification:childern

--- htdocs/src/Controller/Services/Rest/ClassificationController.php
+++ htdocs/src/Controller/Services/Rest/ClassificationController.php
@@ -171,7 +171,7 @@ class ClassificationController extends AbstractFOSRestController
         ]
     )]
     #[Route('/services/rest/classification/nameReferences/{taxonID}.{_format}', name: "services_rest_classification_nameReferences", defaults: ['_format' => 'json'], methods: ['GET'])]
-    public function nameReferences(int $taxonID, #[MapQueryParameter] int $excludeReferenceId = 0, #[MapQueryParameter] int $insertSeries = 0): Response
+    public function nameReferences(int $taxonID, #[MapQueryParameter] ?int $excludeReferenceId = 0, #[MapQueryParameter] ?int $insertSeries = 0): Response
     {
         $data = $this->referenceService->getNameReferences($taxonID, $excludeReferenceId, $insertSeries);
         $view = $this->view($data, 200);
@@ -179,4 +179,100 @@ class ClassificationController extends AbstractFOSRestController
         return $this->handleView($view);
     }
 
+    #[Get(
+        path: '/services/rest/classification/children/{referenceType}/{referenceID}',
+        summary: 'Get classification children of a given taxonID according to a given reference',
+        tags: ['classification'],
+        parameters: [
+            new PathParameter(
+                name: 'referenceType',
+                description: 'Type of reference (citation, person, service, specimen, periodical)',
+                in: 'path',
+                required: true,
+                schema: new Schema(type: 'string'),
+                example: 'periodical'
+            ),
+            new PathParameter(
+                name: 'referenceID',
+                description: 'ID of reference',
+                in: 'path',
+                required: true,
+                schema: new Schema(type: 'integer', nullable: true),
+                example: 70
+            ),
+            new QueryParameter(
+                name: 'taxonID',
+                description: 'optional ID of taxon name',
+                in: 'query',
+                required: false,
+                schema: new Schema(type: 'integer', nullable: true)
+            ),
+            new QueryParameter(
+                name: 'insertSeries',
+                description: 'optional ID of citation-Series to be inserted',
+                in: 'query',
+                required: false,
+                schema: new Schema(type: 'integer', nullable: true)
+            )
+        ],
+        responses: [
+            new \OpenApi\Attributes\Response(
+                response: 200,
+                description: 'fetch a list of all periodicals known to JACQ or returns by ID',
+                content: [new MediaType(
+                    mediaType: 'application/json',
+                    schema: new Schema(
+                        type: 'array',
+                        items: new Items(
+                            properties: [
+                                new Property(property: 'taxonID', description: 'the taxon-ID we asked for', type: 'integer', example: 15),
+                                new Property(property: 'uuid', description: 'URL to UUID service', type: 'object', example: '{"href": "url to get the uuid"}'),
+                                new Property(property: 'referenceId', description: 'ID of reference', type: 'integer', example: 15),
+                                new Property(property: 'referenceName', description: 'name of the reference', type: 'string', example: ''),
+                                new Property(property: 'referenceType', description: 'Type of the reference', type: 'string', example: ''),
+                                new Property(property: 'hasChildren', description: 'true if children of this entry exist', type: 'boolean', example: true),
+                                new Property(property: 'hasType', description: ' true if Typi exist', type: 'boolean', example: false),
+                                new Property(property: 'hasSpecimen', description: 'true if at least one specimen exists', type: 'boolean', example: false),
+                                new Property(property: 'referenceInfo', description: '', type: 'object', example: '{"number": "classification number","order": "classification order","rank_abbr": "rank abbreviation","rank_hierarchy": "rank hierarchy","tax_syn_ID": "internal ID of synonym"}')
+                            ],
+                            type: 'object'
+                        )
+                    )
+                ),
+                    new MediaType(
+                        mediaType: 'application/xml',
+                        schema: new Schema(
+                            type: 'array',
+                            items: new Items(
+                                properties: [
+                                    new Property(property: 'taxonID', description: 'the taxon-ID we asked for', type: 'integer', example: 15),
+                                    new Property(property: 'uuid', description: 'URL to UUID service', type: 'object', example: '{"href": "url to get the uuid"}'),
+                                    new Property(property: 'referenceId', description: 'ID of reference', type: 'integer', example: 15),
+                                    new Property(property: 'referenceName', description: 'name of the reference', type: 'string', example: ''),
+                                    new Property(property: 'referenceType', description: 'Type of the reference', type: 'string', example: ''),
+                                    new Property(property: 'hasChildren', description: 'true if children of this entry exist', type: 'boolean', example: true),
+                                    new Property(property: 'hasType', description: ' true if Typi exist', type: 'boolean', example: false),
+                                    new Property(property: 'hasSpecimen', description: 'true if at least one specimen exists', type: 'boolean', example: false),
+                                    new Property(property: 'referenceInfo', description: '', type: 'object', example: '{"number": "classification number","order": "classification order","rank_abbr": "rank abbreviation","rank_hierarchy": "rank hierarchy","tax_syn_ID": "internal ID of synonym"}')
+                                ],
+                                type: 'object'
+                            )
+                        )
+                    )
+                ]
+            ),
+            new \OpenApi\Attributes\Response(
+                response: 400,
+                description: 'Bad Request'
+            )
+        ]
+    )]
+    #[Route('/services/rest/classification/children/{referenceType}/{referenceID}.{_format}', name: "services_rest_classification_children", defaults: ['_format' => 'json'], methods: ['GET'])]
+    public function children(string $referenceType, int $referenceID, #[MapQueryParameter] ?int $taxonID = 0, #[MapQueryParameter] ?int $insertSeries = 0): Response
+    {
+        $data = $this->referenceService->getChildren($referenceType, $referenceID, $taxonID, $insertSeries);
+        $view = $this->view($data, 200);
+
+        return $this->handleView($view);
+    }
 }
--- htdocs/src/Service/ReferenceService.php
+++ htdocs/src/Service/ReferenceService.php
@@ -14,7 +14,13 @@ readonly class ReferenceService
     {
     }
 
-
+    /**
+     * Fetch a list of all references (which have a classification attached)
+     *
+     * @param string $referenceType Type of references to return (citation, person, service, specimen, periodical)
+     * @param ?int $referenceID optional ID of reference
+     * @return array References information
+     */
     public function getByType(string $referenceType, ?int $referenceID): array
     {
 //            //TODO  these three from oirignal code are not supported or do not exists?
@@ -52,7 +58,6 @@ readonly class ReferenceService
             return $this->entityManager->getConnection()->executeQuery($sql)->fetchAllAssociative();
         }
 
-
     }
 
     protected function getPeriodicalReferences(?int $referenceID): array
@@ -84,7 +89,7 @@ readonly class ReferenceService
     }
 
     /**
-     * TODO refactoring mostly syntax - having no idea what is going on :-)
+     * Return (other) references for this name which include them in their classification
      */
     public function getNameReferences(int $taxonID, int $excludeReferenceId = 0, int $insertSeries = 0): array
     {
@@ -119,16 +124,15 @@ readonly class ReferenceService
         foreach ($dbRows as $dbRow) {
             // check for exclude id
             if ($dbRow['referenceId'] != $excludeReferenceId) {
-                $results[] = array(
+                $results[] = [
                     "referenceName" => $dbRow['referenceName'],
                     "referenceId" => intval($dbRow['referenceId']),
-                    "referenceType" => "citation",
-                    "taxonID" => $taxonID,
+                    "referenceType" => "citation", "taxonID" => $taxonID,
                     "uuid" => array('href' => $this->router->generate('services_rest_scinames_uuid', ['taxonID' => $taxonID], UrlGeneratorInterface::ABSOLUTE_URL)),
                     "hasChildren" => $this->hasClassificationChildren($taxonID, $dbRow['referenceId']),
                     "hasType" => false, //TODO always false?
                     "hasSpecimen" => false //TODO always false?
-                );
+                ];
             }
         }
 
@@ -153,20 +157,37 @@ readonly class ReferenceService
 
         foreach ($dbSyns as $dbSyn) {
             if ($this->isAcceptedTaxonPartOfClassification($dbSyn['referenceId'], $dbSyn['acceptedId'])) {
-                $results[] = array(
-                    "referenceName" => '= ' . $dbSyn['referenceName'],  //  mark the reference Name as synonym
-                    "referenceId" => intval($dbSyn['referenceId']),
-                    "referenceType" => "citation",
-                    "taxonID" => $taxonID,
-                    "uuid" => array('href' => $this->router->generate('services_rest_scinames_uuid', ['taxonID' => $taxonID], UrlGeneratorInterface::ABSOLUTE_URL)),
-                    "hasChildren" => false,
-                    "hasType" => false,
-                    "hasSpecimen" => false,
-                );
+                $results[] = array("referenceName" => '= ' . $dbSyn['referenceName'],  //  mark the reference Name as synonym
+                    "referenceId" => intval($dbSyn['referenceId']), "referenceType" => "citation", "taxonID" => $taxonID, "uuid" => array('href' => $this->router->generate('services_rest_scinames_uuid', ['taxonID' => $taxonID], UrlGeneratorInterface::ABSOLUTE_URL)), "hasChildren" => false, "hasType" => false, "hasSpecimen" => false,);
             }
         }
 
         return $results;
+    }
+
+    /**
+     * check if there are any classification children of the taxonID according to this reference
+     */
+    protected function hasClassificationChildren(int $taxonID, int $referenceID): bool
+    {
+        $sqlQueryChild = "SELECT ts.taxonID
+                                       FROM tbl_tax_synonymy ts
+                                        LEFT JOIN tbl_tax_classification tc ON ts.tax_syn_ID = tc.tax_syn_ID
+                                       WHERE ts.source_citationID = :referenceID
+                                        AND ts.acc_taxon_ID IS NULL
+                                        AND tc.parent_taxonID = :taxonID";
+        $child = $this->entityManager->getConnection()->executeQuery($sqlQueryChild, ['taxonID' => $taxonID, 'referenceID' => $referenceID])->fetchAssociative();
+        if ($child !== false) {
+            $hasChildren = true;
+        } else {
+            $sqlQueryChild = "SELECT ts.taxonID
+                                       FROM tbl_tax_synonymy ts
+                                       WHERE ts.source_citationID = :referenceID
+                                        AND ts.acc_taxon_ID = $taxonID";
+            $child = $this->entityManager->getConnection()->executeQuery($sqlQueryChild, ['referenceID' => $referenceID])->fetchAssociative();
+            $hasChildren = (bool)$child;
+        }
+        return $hasChildren;
     }
 
     /**
@@ -193,27 +214,206 @@ readonly class ReferenceService
     }
 
     /**
-     * check if there are any classification children of the taxonID according to this reference
+     * Get classification children of a given taxonID according to a given reference
+     *
+     * @param string $referenceType Type of reference (periodical, citation, service, etc.)
+     * @param int $referenceID ID of reference
+     * @param int $taxonID optional ID of taxon
+     * @param int $insertSeries optional ID of cication-Series to insert
+     * @return array structured array with classification information
      */
-    protected function hasClassificationChildren(int $taxonID, int $referenceID): bool
+    public function getChildren(string $referenceType, int $referenceID, int $taxonID, int $insertSeries): array
     {
-        $sqlQueryChild = "SELECT ts.taxonID
-                                       FROM tbl_tax_synonymy ts
-                                        LEFT JOIN tbl_tax_classification tc ON ts.tax_syn_ID = tc.tax_syn_ID
-                                       WHERE ts.source_citationID = :referenceID
-                                        AND ts.acc_taxon_ID IS NULL
-                                        AND tc.parent_taxonID = :taxonID";
-        $child = $this->entityManager->getConnection()->executeQuery($sqlQueryChild, ['taxonID' => $taxonID, 'referenceID' => $referenceID])->fetchAssociative();
-        if ($child !== false) {
-            $hasChildren = true;
-        } else {
-            $sqlQueryChild = "SELECT ts.taxonID
-                                       FROM tbl_tax_synonymy ts
-                                       WHERE ts.source_citationID = :referenceID
-                                        AND ts.acc_taxon_ID = $taxonID";
-            $child = $this->entityManager->getConnection()->executeQuery($sqlQueryChild, ['referenceID' => $referenceID])->fetchAssociative();
-            $hasChildren = (bool) $child;
+        $results = array();
+
+        switch ($referenceType) {
+            case 'periodical':
+                $dbRows = $this->getPeriodicalChildrenReferences($referenceID);
+                foreach ($dbRows as $dbRow) {
+                    $results[] = array(
+                        "taxonID" => 0,
+                        "referenceId" => intval($dbRow['referenceID']),
+                        "referenceName" => $dbRow['referenceName'],
+                        "referenceType" => "citation",
+                        "hasChildren" => true,
+                        "hasType" => false,
+                        "hasSpecimen" => false,
+                        "insertedCitation" => false,
+                    );
+                }
+                return $results;
+            case 'citation':
+            default:
+                $dbRows = $this->getCitationChildrenReferences($referenceID, $taxonID);
+                foreach ($dbRows as $dbRow) {
+                    $results[] = array(
+                        "taxonID" => intval($dbRow['taxonID']),
+                        "uuid" => array('href' => $this->router->generate('services_rest_scinames_uuid', ['taxonID' => $taxonID], UrlGeneratorInterface::ABSOLUTE_URL)),
+                        "referenceId" => $referenceID,
+                        "referenceName" => $dbRow['scientificName'],
+                        "referenceType" => "citation",
+                        "hasChildren" => ($dbRow['hasChildren'] > 0 || $dbRow['hasSynonyms'] > 0 || $dbRow['hasBasionym']),
+                        "hasType" => $this->hasType($dbRow['taxonID']),
+                        "hasSpecimen" => $this->hasSpecimen($dbRow['taxonID']),
+                        "insertedCitation" => false,
+                        "referenceInfo" => array(
+                            "number" => $dbRow['number'],
+                            "order" => intval($dbRow['order']),
+                            "rank_abbr" => $dbRow['rank_abbr'],
+                            "rank_hierarchy" => intval($dbRow['rank_hierarchy']),
+                            "tax_syn_ID" => intval($dbRow['tax_syn_ID']),
+                        ),
+                    );
+                    $insertedCitations = $this->getInsertedCitation($insertSeries, $referenceID, $dbRow['taxonID']);
+                    if (!empty($insertedCitations)) {
+                        foreach ($insertedCitations as $citation) {
+                            $results[] = array(
+                                "taxonID" => $citation['taxonID'],
+                                "uuid" => array('href' => $this->router->generate('services_rest_scinames_uuid', ['taxonID' => $taxonID], UrlGeneratorInterface::ABSOLUTE_URL)),
+                                "referenceId" => $citation['referenceId'],
+                                "referenceName" => $citation['referenceName'],
+                                "referenceType" => $citation['referenceType'],
+                                "hasChildren" => $citation['hasChildren'],
+                                "insertedCitation" => true,
+                            );
+                        }
+                    }
+                }
+                break;
         }
-        return $hasChildren;
+
+        return $results;
+    }
+
+    /**
+     * get all citations which belong to the given periodical
+     */
+    protected function getPeriodicalChildrenReferences(int $referenceID): array
+    {
+
+        $sql = "SELECT `herbar_view`.GetProtolog(l.citationID) AS referenceName, l.citationID AS referenceID
+                    FROM tbl_lit l
+                     LEFT JOIN tbl_tax_synonymy ts ON ts.source_citationID = l.citationID
+                     LEFT JOIN tbl_tax_classification tc ON tc.tax_syn_ID = ts.tax_syn_ID
+                    WHERE ts.tax_syn_ID IS NOT NULL
+                     AND tc.classification_id IS NOT NULL
+                     AND l.periodicalID = :referenceID
+                    GROUP BY ts.source_citationID
+                    ORDER BY referenceName";
+        return $this->entityManager->getConnection()->executeQuery($sql, ['referenceID' => $referenceID])->fetchAllAssociative();
+
+    }
+
+    /**
+     * get all citations which belong to the given citation
+     */
+    protected function getCitationChildrenReferences(int $referenceID, int $taxonID): array
+    {
+        $sql = "SELECT `herbar_view`.GetScientificName( ts.`taxonID`, 0 ) AS `scientificName`,
+                           ts.taxonID,
+                           ts.tax_syn_ID AS `tax_syn_ID`,
+                           tc.`number` AS `number`,
+                           tc.`order` AS `order`,
+                           tr.rank_abbr,
+                           tr.rank_hierarchy,
+                           MAX(`has_children`.`tax_syn_ID` IS NOT NULL) AS `hasChildren`,
+                           MAX(`has_synonyms`.`tax_syn_ID` IS NOT NULL) AS `hasSynonyms`,
+                           (`has_basionym`.`basID`         IS NOT NULL) AS `hasBasionym`
+                    FROM tbl_tax_synonymy ts
+                     LEFT JOIN tbl_tax_species tsp ON ts.taxonID = tsp.taxonID
+                     LEFT JOIN tbl_tax_rank tr ON tsp.tax_rankID = tr.tax_rankID
+                     LEFT JOIN tbl_tax_classification tc ON ts.tax_syn_ID = tc.tax_syn_ID
+                     LEFT JOIN tbl_tax_synonymy has_synonyms ON (has_synonyms.acc_taxon_ID = ts.taxonID
+                                                                 AND has_synonyms.source_citationID = ts.source_citationID)
+                     LEFT JOIN tbl_tax_classification has_children_clas ON has_children_clas.parent_taxonID = ts.taxonID
+                     LEFT JOIN tbl_tax_synonymy has_children ON (has_children.tax_syn_ID = has_children_clas.tax_syn_ID
+                                                                 AND has_children.source_citationID = ts.source_citationID)
+                     LEFT JOIN tbl_tax_species has_basionym ON ts.taxonID = has_basionym.taxonID
+                    WHERE ts.source_citationID = :referenceID
+                     AND ts.acc_taxon_ID IS NULL ";
+
+
+        // check if we search for children of a specific taxon
+        if ($taxonID > 0) {
+            $sql .= " AND tc.parent_taxonID = :taxonID ";
+        } // .. if not make sure we only return entries which have at least one child
+        else {
+            $sql .= " AND tc.parent_taxonID IS NULL
+                          AND has_children.tax_syn_ID IS NOT NULL";
+        }
+        $sql .= " GROUP BY ts.taxonID ORDER BY `order`, `scientificName`";
+
+        return $this->entityManager->getConnection()->executeQuery($sql, ['referenceID' => $referenceID, 'taxonID' => $taxonID])->fetchAllAssociative();
+
+    }
+
+    /**
+     * Are there any type records of a given taxonID?
+     *
+     * @param int $taxonID ID of taxon
+     */
+    protected function hasType(int $taxonID): bool
+    {
+        $sql = "SELECT s.specimen_ID
+                FROM tbl_specimens s
+                 LEFT JOIN tbl_specimens_types tst ON tst.specimenID = s.specimen_ID
+                WHERE tst.typusID IS NOT NULL
+                 AND tst.taxonID = :taxonID";
+        return (bool)$this->entityManager->getConnection()->executeQuery($sql, ['taxonID' => $taxonID])->fetchAssociative();
+    }
+
+    /**
+     * Are there any specimen records of a given taxonID?
+     *
+     * @param int $taxonID ID of taxon
+     * @return bool specimen record(s) present?
+     */
+    protected function hasSpecimen(int $taxonID): bool
+    {
+        $sql = "SELECT specimen_ID FROM tbl_specimens WHERE taxonID = :taxonID";
+        return (bool)$this->entityManager->getConnection()->executeQuery($sql, ['taxonID' => $taxonID])->fetchAssociative();
+    }
+
+    protected function getInsertedCitation(int $insertSeries, int $referenceID, int $taxonID): array
+    {
+        $results = [];
+
+        $dbRows = $this->findCitations($insertSeries, $referenceID, $taxonID);
+        foreach ($dbRows as $row) {
+            $results[] = array(
+                'referenceName' => $this->getCitationName($row['citationID']),
+                'referenceId' => intval($row['citationID']),
+                "referenceType" => "citation",
+                "taxonID" => $taxonID,
+                "uuid" => array('href' => $this->router->generate('services_rest_scinames_uuid', ['taxonID' => $taxonID], UrlGeneratorInterface::ABSOLUTE_URL)),
+                "hasChildren" => $this->hasClassificationChildren($taxonID, $row['citationID']),
+            );
+        }
+
+        return $results;
+    }
+
+    protected function findCitations(int $insertSeries, int $referenceID, int $taxonID): array
+    {
+        $sql = "SELECT citationID
+                FROM tbl_classification_citation_insert
+                WHERE series = :insertSeries
+                 AND taxonID = :taxonID
+                 AND referenceId = :referenceID
+                ORDER BY sequence";
+        return $this->entityManager->getConnection()->executeQuery($sql, ['taxonID' => $taxonID, 'insertSeries' => $insertSeries, 'referenceID' => $referenceID])->fetchAllAssociative();
+
+    }
+
+    protected function getCitationName(int $id): ?string
+    {
+        $sql = "SELECT `herbar_view`.GetProtolog(`citationID`) AS `referenceName`
+                                               FROM `tbl_lit`
+                                               WHERE `citationID` = :id";
+        $name = $this->entityManager->getConnection()->executeQuery($sql, ['id' => $id])->fetchOne();
+        if ($name === false) {
+            return null;
+        }
+        return $name;
     }
 }
