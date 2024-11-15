c33337b6b33934f5d52b546f562bf9d9426df77a
Petr Novotný <krkabol@gmail.com>
2024-11-15 09:23:16 +0100

    rest stableidentificators:errors

--- htdocs/src/Controller/Services/Rest/StableIdentifierController.php
+++ htdocs/src/Controller/Services/Rest/StableIdentifierController.php
@@ -9,8 +9,10 @@ use OpenApi\Attributes\Items;
 use OpenApi\Attributes\MediaType;
 use OpenApi\Attributes\PathParameter;
 use OpenApi\Attributes\Property;
+use OpenApi\Attributes\QueryParameter;
 use OpenApi\Attributes\Schema;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StableIdentifierController extends AbstractFOSRestController
@@ -121,4 +123,243 @@ class StableIdentifierController extends AbstractFOSRestController
         return $this->handleView($view);
     }
 
+    #[Get(
+        path: '/services/rest/stableIdentifier/resolve/{sid}',
+        summary: 'Get specimen-id, valid stable identifier and all stable identifiers of a given stable identifier. ',
+        tags: ['stable identifier'],
+        parameters: [
+            new PathParameter(
+                name: 'sid',
+                description: 'stable identifier of specimen',
+                in: 'path',
+                required: true,
+                schema: new Schema(type: 'string'),
+                example: 'https://wu.jacq.org/WU-0000264'
+            )
+        ],
+        responses: [
+            new \OpenApi\Attributes\Response(
+                response: 200,
+                description: 'List of taxa names',
+                content: [new MediaType(
+                    mediaType: 'application/json',
+                    schema: new Schema(
+                        type: 'array',
+                        items: new Items(
+                            properties: [
+                                new Property(property: 'specimenID', description: 'ID of specimen', type: 'integer', example: 6830
+                                ),
+                                new Property(property: 'stableIdentifierLatest', description: 'Latest stable identifier', properties: [
+                                    new Property(property: 'stableIdentifier',description: 'Stable identifier',type: 'string'),
+                                    new Property(property: 'timestamp',description: 'Timestamp of the stable identifier',type: 'string',format: 'date-time'),
+                                    new Property(property: 'link',description: 'Link to details page of JACQ (for convenience)',type: 'string',format: 'uri'),
+                                ],
+                                    type: 'object'
+                                ),
+                                new Property(
+                                    property: 'stableIdentifierList',description: 'List of all found stable identifiers, ordered by timestamp',type: 'array',
+                                    items: new Items(
+                                        properties: [
+                                            new Property(property: 'stableIdentifier',description: 'Stable identifier',type: 'string'),
+                                            new Property(property: 'timestamp',description: 'Timestamp of the stable identifier',type: 'string',format: 'date-time'),
+                                        ],
+                                        type: 'object'
+                                    )
+                                ),
+                            ],
+                            type: 'object'
+                        )
+                    )
+                )
+                ]
+            ),
+            new \OpenApi\Attributes\Response(
+                response: 400,
+                description: 'Bad Request'
+            )
+        ]
+    )]
+    #[Route('/services/rest/stableIdentifier/resolve/{sid}', requirements: ['sid' => '^https:\/\/(?:[a-zA-Z0-9.-]+\.)?jacq\.org\/[A-Za-z0-9\-]+$'], methods: ['GET'])]
+    public function resolve(string $sid): Response
+    {
+        //TODO removed the "withRedirect" option in OPenApi, solving by "nonvisible" forward inside the framework
+        $sid = urldecode($sid);
+        $specimenID = $this->specimenService->getSpecimenIDfromSid($sid);
+        return $this->forward(self::class.'::sid', ['specimenID' => $specimenID]);
+    }
+
+    #[Get(
+        path: '/services/rest/stableIdentifier/errors',
+        summary: 'get a list of all errors which prevent the generation of stable identifier',
+        tags: ['stable identifier'],
+        parameters: [
+            new QueryParameter(
+                name: 'sourceID',
+                description: 'optional ID of source to check (default=all sources)',
+                in: 'query',
+                required: false,
+                schema: new Schema(type: 'integer')
+            )
+        ],
+        responses: [
+            new \OpenApi\Attributes\Response(
+                response: 200,
+                description: 'List of taxa names',
+                content: [new MediaType(
+                    mediaType: 'application/json',
+                    schema: new Schema(
+                        type: 'array',
+                        items: new Items(
+                            properties: [
+                                new Property(
+                                    property: 'total',
+                                    description: 'Total number of records found',
+                                    type: 'integer'
+                                ),
+                                new Property(
+                                    property: 'result',
+                                    description: 'List of found entries',
+                                    type: 'array',
+                                    items: new Items(
+                                        properties: [
+                                            new Property(
+                                                property: 'specimenID',
+                                                description: 'ID of specimen',
+                                                type: 'integer'
+                                            ),
+                                            new Property(
+                                                property: 'link',
+                                                description: 'Link to details-page of JACQ (for convenience)',
+                                                type: 'string',
+                                                format: 'uri'
+                                            ),
+                                            new Property(
+                                                property: 'errorList',
+                                                description: 'List of errors and existing stable identifiers (if any) for this specimen-ID',
+                                                type: 'array',
+                                                items: new Items(
+                                                    properties: [
+                                                        new Property(
+                                                            property: 'stableIdentifier',
+                                                            description: 'Stable identifier (if it exists) or null',
+                                                            type: 'string',
+                                                            nullable: true
+                                                        ),
+                                                        new Property(
+                                                            property: 'timestamp',
+                                                            description: 'Timestamp of creation',
+                                                            type: 'string',
+                                                            format: 'date-time'
+                                                        ),
+                                                        new Property(
+                                                            property: 'error',
+                                                            description: 'The error description',
+                                                            type: 'string'
+                                                        ),
+                                                        new Property(
+                                                            property: 'link',
+                                                            description: 'Link to details-page of JACQ of the blocking specimen (if present)',
+                                                            type: 'string',
+                                                            format: 'uri',
+                                                            nullable: true
+                                                        ),
+                                                    ],
+                                                    type: 'object'
+                                                )
+                                            ),
+                                        ],
+                                        type: 'object'
+                                    )
+                                ),
+                            ],
+                            type: 'object'
+                        )
+                    )
+                ),
+                    new MediaType(
+                        mediaType: 'application/xml',
+                        schema: new Schema(
+                            type: 'array',
+                            items: new Items(
+                                properties: [
+                                    new Property(
+                                        property: 'total',
+                                        description: 'Total number of records found',
+                                        type: 'integer'
+                                    ),
+                                    new Property(
+                                        property: 'result',
+                                        description: 'List of found entries',
+                                        type: 'array',
+                                        items: new Items(
+                                            properties: [
+                                                new Property(
+                                                    property: 'specimenID',
+                                                    description: 'ID of specimen',
+                                                    type: 'integer'
+                                                ),
+                                                new Property(
+                                                    property: 'link',
+                                                    description: 'Link to details-page of JACQ (for convenience)',
+                                                    type: 'string',
+                                                    format: 'uri'
+                                                ),
+                                                new Property(
+                                                    property: 'errorList',
+                                                    description: 'List of errors and existing stable identifiers (if any) for this specimen-ID',
+                                                    type: 'array',
+                                                    items: new Items(
+                                                        properties: [
+                                                            new Property(
+                                                                property: 'stableIdentifier',
+                                                                description: 'Stable identifier (if it exists) or null',
+                                                                type: 'string',
+                                                                nullable: true
+                                                            ),
+                                                            new Property(
+                                                                property: 'timestamp',
+                                                                description: 'Timestamp of creation',
+                                                                type: 'string',
+                                                                format: 'date-time'
+                                                            ),
+                                                            new Property(
+                                                                property: 'error',
+                                                                description: 'The error description',
+                                                                type: 'string'
+                                                            ),
+                                                            new Property(
+                                                                property: 'link',
+                                                                description: 'Link to details-page of JACQ of the blocking specimen (if present)',
+                                                                type: 'string',
+                                                                format: 'uri',
+                                                                nullable: true
+                                                            ),
+                                                        ],
+                                                        type: 'object'
+                                                    )
+                                                ),
+                                            ],
+                                            type: 'object'
+                                        )
+                                    ),
+                                ],
+                                type: 'object'
+                            )
+                        )
+                    )
+                ]
+            ),
+            new \OpenApi\Attributes\Response(
+                response: 400,
+                description: 'Bad Request'
+            )
+        ]
+    )]
+    #[Route('/services/rest/stableIdentifier/errors.{_format}', defaults: ['_format' => 'json'],  methods: ['GET'])]
+    public function errors(#[MapQueryParameter] ?int $sourceID): Response
+    {
+        $results = $this->specimenService->getEntriesWithErrors($sourceID);
+        $view = $this->view($results, 200);
+        return $this->handleView($view);
+    }
 }
--- htdocs/src/Service/SpecimenService.php
+++ htdocs/src/Service/SpecimenService.php
@@ -43,4 +43,63 @@ readonly class SpecimenService
         return $ret;
     }
 
+
+    /**
+     * get specimen-id of a given stable identifier
+     */
+    public function getSpecimenIDfromSid(string $sid): ?int
+    {
+        $pos = strpos($sid, "JACQID");
+        if ($pos !== false) {  // we've found a sid with JACQID in it, so check the attached specimen-ID and return it, if valid
+            //todo - hard coded 6 digits of ID
+            $specimenID = intval(substr($sid, $pos + 6));
+            $sql = "SELECT specimen_ID
+                             FROM tbl_specimens
+                             WHERE specimen_ID = :specimenID";
+            $id = $this->entityManager->getConnection()->executeQuery($sql, ['specimenID' => $specimenID])->fetchOne();
+        } else {
+            $sql = "SELECT specimen_ID
+                             FROM tbl_specimens_stblid
+                             WHERE stableIdentifier = :sid";
+            $id = $this->entityManager->getConnection()->executeQuery($sql, ['sid' => $sid])->fetchOne();
+        }
+        if ($id === null || $id === false) {
+            return null;
+        }
+        return $id;
+    }
+
+
+    /**
+     * get a list of all errors which prevent the generation of stable identifier
+     */
+    public function getEntriesWithErrors(?int $sourceID = null): array
+    {
+        if ($sourceID !== null) {
+            $sql = "SELECT ss.specimen_ID AS specimenID, CONCAT('https://www.jacq.org/detail.php?ID=', ss.specimen_ID) AS link
+                              FROM tbl_specimens_stblid ss
+                               JOIN tbl_specimens s ON ss.specimen_ID = s.specimen_ID
+                               JOIN tbl_management_collections mc ON s.collectionID = mc.collectionID
+                              WHERE ss.stableIdentifier IS NULL
+                               AND mc.source_id = :sourceID
+                              GROUP BY ss.specimen_ID
+                              ORDER BY ss.specimen_ID";
+            $rows = $this->entityManager->getConnection()->executeQuery($sql, ['sourceID' => $sourceID])->fetchAllAssociative();
+        } else {
+            $sql = "SELECT specimen_ID AS specimenID, CONCAT('https://www.jacq.org/detail.php?ID=', specimen_ID) AS link
+                              FROM tbl_specimens_stblid
+                              WHERE stableIdentifier IS NULL
+                              GROUP BY specimen_ID
+                              ORDER BY specimen_ID";
+               $rows = $this->entityManager->getConnection()->executeQuery($sql)->fetchAllAssociative();
+        }
+        $data = array('total' => count($rows));
+        foreach ($rows as $line => $row) {
+            $data['result'][$line] = $row;
+            $data['result'][$line]['errorList'] = $this->getAllStableIdentifiers($row['specimenID'])['list'];
+        }
+
+        return $data;
+    }
+
 }
