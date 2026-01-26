a02c1e5ea48813071f1d48309a39e0b193fddd15
Petr Novotný <krkabol@gmail.com>
2026-01-26 11:33:07 +0100

    fix icon

--- htdocs/assets/js/institutionMap.js
+++ htdocs/assets/js/institutionMap.js
@@ -3,19 +3,27 @@ import 'leaflet.markercluster/dist/leaflet.markercluster';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'leaflet/dist/leaflet.css';
 
+// fix pro produkci
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 let mapInstance = null;
 export default function institutionMap() {
+    const mapContainer = document.getElementById('map-institutions');
 
-        const mapContainer = document.getElementById('map-institutions');
+    mapInstance = L.map('map-institutions').setView([50.0755, 14.4378], 2);
 
-        mapInstance = L.map('map-institutions').setView([50.0755, 14.4378], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors',
+    }).addTo(mapInstance);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '© OpenStreetMap contributors',
-        }).addTo(mapInstance);
-
-        const institutions = JSON.parse(mapContainer.dataset.institutions);
+    const institutions = JSON.parse(mapContainer.dataset.institutions);
     institutions.forEach(inst => {
         L.marker([inst.lat, inst.lon])
             .addTo(mapInstance)
@@ -24,6 +32,4 @@ export default function institutionMap() {
                 `<a href="${inst.link}" target="_blank">${inst.link}</a>`
             );
     });
-
-
 }
